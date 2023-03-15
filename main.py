24241368af396bf7b284a2b5a76114c09a4cbbff
Jully Silva <jkasilva@sga.pucminas.br>
2023-03-15 14:05:09 -0300

    criando documento com os githubs

--- main.py
+++ main.py
@@ -1,24 +1,66 @@
-import csv
-import os
+from graphqlclient import GraphQLClient
+import pandas as pd
+import json
+import time
 
-# Caminho do arquivo CSV
-caminho_arquivo = 'arquivo.csv'
+API = 'https://api.github.com/graphql'
+TOKEN = 'token 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'
 
-# Pasta onde o repositório será clonado
-caminho_destino = 'pasta_destino'
+args ={'after': None}
 
-# Abre o arquivo CSV
-with open(caminho_arquivo, 'r') as csv_file:
-    # Lê o arquivo CSV com um objeto reader
-    csv_reader = csv.reader(csv_file)
+query =  """
+    query ($after: String) {
+  search(query: "stars:>100 language:Java", type: REPOSITORY, first: 10, after: $after) {
+    pageInfo {
+      hasPreviousPage
+      hasNextPage
+      startCursor
+      endCursor
+    }
+    edges {
+      node {
+        ... on Repository {
+          id
+          url
+          name
+          nameWithOwner
+          stargazerCount
+          createdAt
+        }
+      }
+    }
+  }
+}
+    """
 
-    # Pula a primeira linha do arquivo CSV (cabeçalho)
-    next(csv_reader)
 
-    # Loop através de cada linha do arquivo CSV
-    for linha in csv_reader:
-        # Extrai a coluna "url" da linha atual
-        url = linha[0]
 
-        # Clona o repositório com base na URL usando o comando git clone
-        os.system('git clone ' + url + ' ' + caminho_destino)
+client = GraphQLClient(API)
+client.inject_token(TOKEN)
+
+df = pd.DataFrame()
+
+for i in range(100):
+    response = json.loads(client.execute(query= query, variables=args))
+    print(response)
+    args['after'] = response['data']['search']['pageInfo']['endCursor']
+
+    data = response['data']['search']['edges']
+    df = pd.concat([df, pd.json_normalize(data)], ignore_index=True)
+
+    time.sleep(1)
+    
+
+
+# Renomeia as colunas do dataframe
+df.columns = ['ID',
+              'URL',
+              'Name',
+              'nameWithOwner',
+              'Stargazers',
+              'createdAt']
+
+# df = df.drop(df.columns[-1], axis=1)
+
+# Salva o dataframe em um arquivo CSV
+df.to_csv('repositorios.csv', sep=';', index=False)
